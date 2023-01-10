97df2986e032d8421ec41bbf6410e722a8cdf202
Kyle Chase <thekylechase@gmail.com>
2023-01-10 12:48:09 -0700

    Finalize scripting functions
    Increment version to 1.1.0

--- ntfy-gateway/src/main/java/com/kyvislabs/ntfy/gateway/profile/NtfyNotificationProfile.java
+++ ntfy-gateway/src/main/java/com/kyvislabs/ntfy/gateway/profile/NtfyNotificationProfile.java
@@ -32,17 +32,10 @@ import com.kyvislabs.ntfy.common.NtfyClient;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.time.Duration;
 import java.util.*;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
-import java.util.Base64;
 
 public class NtfyNotificationProfile implements AlarmNotificationProfile {
 
@@ -155,66 +148,6 @@ public class NtfyNotificationProfile implements AlarmNotificationProfile {
                                 title)
                 );
 
-                // var builder = HttpRequest.newBuilder();
-
-                // builder = builder
-                //         .uri(URI.create(ntfyUrl))
-                //         .header("Content-Type", "application/json")
-                //         .timeout(Duration.ofSeconds(10))
-                //         .POST(HttpRequest.BodyPublishers.ofString(message));
-
-                // if (!StringUtils.isBlank(username) && !StringUtils.isBlank(password)){
-                //     String valueToEncode = username + ":" + password;
-                //     builder.header("Authentication", "Basic " + Base64.getEncoder().encodeToString(valueToEncode.getBytes()));
-                // }
-
-                // if (!StringUtils.isBlank(title)) {
-                //     builder.header("Title", title);
-                // }
-
-                // if (!StringUtils.isBlank(tags)) {
-                //     builder.header("Tags", tags);
-                // }
-
-                // if (!StringUtils.isBlank(priority)) {
-                //     builder.header("Priority", priority);
-                // }
-
-                // if (!StringUtils.isBlank(clickAction)) {
-                //     builder.header("Click", clickAction);
-                // }
-
-                // if (!StringUtils.isBlank(attach)) {
-                //     builder.header("Attach", attach);
-                // }
-
-                // if (!StringUtils.isBlank(actions)) {
-                //     String[] parts = StringUtils.split(actions,";");
-                //     if (parts.length > 3){
-                //         parts = Arrays.copyOfRange(parts,0,3);
-                //         actions = StringUtils.join(parts, ";");
-                //     }
-                //     builder.header("Action", actions);
-                // }
-
-                // if (!StringUtils.isBlank(icon)) {
-                //     builder.header("Icon", icon);
-                // }
-
-                // final var request = builder.build();
-                // try {
-
-                //     final var response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-                //     if (!(response.statusCode() >= 200 && response.statusCode() <= 399)) {
-                //         logger.error("Error sending notification: status code=" + response.statusCode() + ", response=" + response.body());
-                //     }
-                // } catch (IOException e) {
-                //     logger.error("Unable to send notification", e);
-                //     success = false;
-                // } catch (InterruptedException e) {
-                //     logger.error("Unable to send notification", e);
-                //     success = false;
-                // }
                 success = ntfyClient.sendMessage(serverUrl, topic, message, title, tags, priority, clickAction, attach, actions, icon, username, password);
                 audit(success, String.format("Ntfy message to topic %s", topic), notificationContext);
             }
